a6cd175d88b1b6a86127992fbd6903d0e1f063e8
Jcpacheco376 <jcpacheco3760@gmail.com>
2025-10-16 13:41:46 -0700

    x

--- CONTROL-DE-ASISTENCIA-API/src/config/index.ts
+++ CONTROL-DE-ASISTENCIA-API/src/config/index.ts
@@ -1,19 +1,3 @@
-// import dotenv from 'dotenv';
-
-
-// dotenv.config();
-// /*
-// export const PORT = process.env.PORT || 3001;
-// export const JWT_SECRET = process.env.JWT_SECRET || 'TU_SECRETO_JWT_SUPER_SEGURO_CAMBIAME';
-// export const LOCAL_IP = process.env.LOCAL_IP || '1x92.168.0.10';
-// */
-// export const PORT =  3001;
-// export const JWT_SECRET = 'TU_SECRETO_JWT_SUPER_SEGURO_CAMBIAME';
-// export const LOCAL_IP =  '192.168.0.10';
-
-
-// export const CORS_ORIGIN = process.env.CORS_ORIGIN;
-
 import dotenv from 'dotenv';
 
 dotenv.config();
--- CONTROL-DE-ASISTENCIA-API/src/index.ts
+++ CONTROL-DE-ASISTENCIA-API/src/index.ts
@@ -1,20 +1,3 @@
-// import express from 'express';
-// import cors from 'cors';
-// import { PORT, LOCAL_IP } from './config';
-
-// import apiRouter from './api/routes';
-
-// const app = express();
-
-// app.use(cors({ origin: `http://${LOCAL_IP}:5173` }));
-// app.use(express.json());
-
-// app.use('/api', apiRouter);
-
-// app.listen(PORT, '0.0.0.0', () => {
-//     console.log(`🚀 Servidor de la API corriendo en http://${LOCAL_IP}:${PORT}`);
-// });
-
 
 import express from 'express';
 import cors from 'cors';
