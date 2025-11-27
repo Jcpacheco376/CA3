bce63866ae467f625c79a2d38612ee40460fa726
Jcpacheco376 <jcpacheco3760@gmail.com>
2025-11-27 14:15:51 -0700

    Guardando cambios finales en controladores y script de despliegue

--- deploy.bat
+++ deploy.bat
@@ -1,15 +1,8 @@
 @echo off
-<<<<<<< HEAD
-echo Leyendo variables de configuracion...
-
-REM Lee el archivo .env.deploy y establece las variables de entorno
-for /f "tokens=1,* delims==" %%A in (.env.deploy) do (
-=======
 setlocal enabledelayedexpansion
 
 echo Leyendo variables de configuracion...
 for /f "usebackq tokens=1,* delims==" %%A in (".env.deploy") do (
->>>>>>> develop
     set "%%A=%%B"
 )
 
@@ -21,20 +14,6 @@ echo Servidor destino: %DEPLOY_HOST%
 echo.
 
 echo --- 1. Copiando Frontend ---
-<<<<<<< HEAD
-robocopy .\CONTROL-DE-ASISTENCIA\dist \\%DEPLOY_HOST%\%FRONTEND_PATH% /E /PURGE
-echo.
-
-echo --- 2. Copiando API ---
-robocopy .\CONTROL-DE-ASISTENCIA-API\dist \\%DEPLOY_HOST%\%API_PATH%\dist /E /PURGE
-robocopy .\CONTROL-DE-ASISTENCIA-API\node_modules \\%DEPLOY_HOST%\%API_PATH%\node_modules /E /PURGE
-copy .\CONTROL-DE-ASISTENCIA-API\package.json \\%DEPLOY_HOST%\%API_PATH%\package.json
-copy .\CONTROL-DE-ASISTENCIA-API\.env.production \\%DEPLOY_HOST%\%API_PATH%\.env
-echo.
-
-echo --- 3. Reiniciando API en el servidor ---
-psexec \\%DEPLOY_HOST% -u %DEPLOY_USER% -p %DEPLOY_PASSWORD% pm2 restart api-asistencia
-=======
 REM Paso 1A: Copia los archivos de la app (dist) y borra los viejos
 robocopy ".\CONTROL-DE-ASISTENCIA\dist" "\\%DEPLOY_HOST%\%FRONTEND_PATH%" /E /PURGE
 REM Paso 1B: Copia el web.config para que no sea borrado por PURGE
@@ -64,7 +43,6 @@ if %errorlevel% neq 0 (
     goto :eof
 )
 echo API iniciada/reiniciada exitosamente.
->>>>>>> develop
 echo.
 
 echo =================================
